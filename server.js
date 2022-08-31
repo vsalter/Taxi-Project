807841dce1a121c4ef96c57fb3ba27885981cb8f
vsalter <vsalter1@student.gsu.edu>
2022-08-31 15:01:37 -0400

    added Database URL. Updated Views. Updated Routes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 require('./config/database');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var taxiRouter = require('./routes/taxi');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/taxi', taxiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
