0406a190202ab3579add07712f5f0407e550ee99
vsalter <vsalter1@student.gsu.edu>
2022-09-02 16:56:16 -0400

    updated index routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Taxi' });
 });
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {scope: ['profile', 'email']}
+));
+
+
+// Google call back route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+      successRedirect: '/taxi',
+      failureRedirect: '/'
+  }
+));
+
+
+// Google logout
+router.get('/logout', function (req, res) {
+  req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+});
+
 
 module.exports = router;
