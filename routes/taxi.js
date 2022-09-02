aef9daed0e6046f668c62d3f984a8fbe83afdd37
vsalter <vsalter1@student.gsu.edu>
2022-09-02 16:53:44 -0400

    updated taxi routes

--- routes/taxi.js
+++ routes/taxi.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const taxiCtrl = require ('../controllers/taxi');
+const isLoggedIn = require('../config/auth');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('taxi/index');
-});
+router.get('/', isLoggedIn, taxiCtrl.index);
+router.get('/new', isLoggedIn, taxiCtrl.new);
+router.get('/:id', isLoggedIn, taxiCtrl.show);
+router.post('/', isLoggedIn, taxiCtrl.create);
+router.delete('/:id', isLoggedIn, taxiCtrl.delete);
+
+router.get('/:id/update', isLoggedIn, taxiCtrl.edit);
+router.put('/:id', isLoggedIn, taxiCtrl.update);
+
 
 module.exports = router;
