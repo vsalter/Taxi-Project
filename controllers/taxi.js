7f222036a6c86c5cb6feabfe4db63c3523209dfd
vsalter <vsalter1@student.gsu.edu>
2022-09-02 17:01:57 -0400

    updated taxi controllers

---
+++ controllers/taxi.js
@@ -0,0 +1,65 @@
+const Taxi = require('../models/taxi');
+const Passenger = require('../models/passenger');
+
+function index(req, res){
+    Taxi.find({}, function(err, taxis){
+        if (err) return res.redirect('/');
+        res.render('taxi/index',{taxis})
+    });
+}
+
+function show(req, res) {
+    Taxi.findById(req.params.id, function(err, taxi) {
+        Passenger.find({taxi: taxi._id}, function(err, passengers){
+            res.render('taxi/show', {title: 'Taxi Detail', taxi, passengers});
+        });
+    });
+}
+
+function newTaxi(req, res){
+    res.render('taxi/new');
+}
+
+function create(req, res){
+    req.body.userName = req.user.name;
+    req.body.userEmail = req.user.email;
+	Taxi.create(req.body);
+	console.log(req.body);
+	res.redirect('/taxi');
+}
+
+function deleteTaxi(req, res, next) {
+    Taxi.findByIdAndDelete(req.params.id, function(err, taxi){
+        res.redirect('/taxi');
+    });
+}
+
+function edit(req, res) {
+    Taxi.findById(req.params.id, function(err, taxi) {
+        Passenger.find({taxi: taxi._id}, function(err, passengers){
+            res.render('taxi/update', {title: 'Edit Trip', taxi, passengers});
+        });
+    });
+  }
+
+  function update(req, res) {
+    Taxi.findOneAndUpdate(
+        {_id: req.params.id},
+        req.body,
+        {new: true},
+        function(err, taxi) {
+            if (err || !taxi) return res.redirect('/taxi');
+            res.redirect(`/taxi/${taxi._id}`);
+          }
+        );
+  }
+
+module.exports = {
+    index,
+    new: newTaxi,
+    create,
+    show,
+    delete: deleteTaxi,
+    edit, 
+    update
+};
